266cb87eb4a9264966c2b694598f40d1acfdb3d0
Yannick Terme <yannickterme@gmail.com>
2024-02-25 09:19:18 +0000

    some changes

--- modules/self_attention.py
+++ modules/self_attention.py
@@ -1,53 +1,52 @@
 from torch import nn
 import numpy as np
 import torch
+from torch.nn import functional as F
 
 
 class SelfAttention(nn.Module):
-    def __init__(self, input_dim, output_dim, masked=False):
+    def __init__(self, input_dim, output_dim, masked=False, attention_dropout=0.1, resid_dropout=0.1):
         super().__init__()
         self.dk = output_dim
-        # self.WQ = nn.Linear(input_dim, output_dim)
-        # self.WK = nn.Linear(input_dim, output_dim)
-        # self.WV = nn.Linear(input_dim, output_dim)
         # initialize matrices WQ, WK, WV with random values
-        self.WQ = nn.Parameter(torch.randn(input_dim, output_dim))
-        self.WK = nn.Parameter(torch.randn(input_dim, output_dim))
-        self.WV = nn.Parameter(torch.randn(input_dim, output_dim))
+        self.WQ = nn.Parameter(nn.init.xavier_uniform_(torch.randn(input_dim, output_dim)))
+        self.WK = nn.Parameter(nn.init.xavier_uniform_(torch.randn(input_dim, output_dim)))
+        self.WV = nn.Parameter(nn.init.xavier_uniform_(torch.randn(input_dim, output_dim)))
         self.softmax = nn.Softmax(dim=1)
+        self.attention_dropout = nn.Dropout(attention_dropout)
+        self.resid_dropout = nn.Dropout(resid_dropout)
         self.masked = masked
-        if self.masked:
-            self.WQ = nn.Parameter(torch.tril(self.WQ))
-            self.WK = nn.Parameter(torch.tril(self.WK))
-            self.WV = nn.Parameter(torch.tril(self.WV))
-            # self.dk = ?
-        # self.encoder_decoder = encoder_decoder
-
-    # def cuda(self, device):
-    #     self.WK.cuda(device)
-    #     self.WV.cuda(device)
-    #     self.WQ.cuda(device)
 
 
+        
 
     def __call__(self, x, y=None):
         # assert self.encoder_decoder == (y is not None)
         q = x @ self.WQ
         k = x @ self.WK
         v = x @ self.WV
-        return self.softmax(q @ torch.transpose(k, 1, 2) / np.sqrt(self.dk)) @ v
+        scores = q @ k.transpose(1, 2) / np.sqrt(self.dk)
+        if self.masked:
+            mask = torch.tril(torch.ones(x.size(1), x.size(1))).to(x.device)
+            scores = scores.masked_fill(mask==0, float('-inf'))
+        attention = F.softmax(scores, dim=-1)
+        scores = self.attention_dropout(scores)
+        y = attention @ v
+        # attention = self.resid_dropout(y)
+        return y
 
 
 class MultiHeadAttention(nn.Module):
-    def __init__(self, input_dim, output_dim, num_heads, masked=False):
+    def __init__(self, input_dim, output_dim, num_heads, masked=False, prob_dropout=0.1):
         super().__init__()
         self.num_heads = num_heads
         self.output_dim = output_dim
         # self.WO = nn.Parameter(torch.randn(num_heads * output_dim, output_dim))
         self.WO = nn.Linear(num_heads * output_dim, output_dim)
-        self.attention = SelfAttention(input_dim, output_dim, masked=masked)
+        self.attentions = nn.ModuleList([SelfAttention(input_dim, output_dim, masked=masked) for _ in range(num_heads)])
+        self.dropout = nn.Dropout(p=prob_dropout)
         self.masked = masked
 
     def __call__(self, x):
-        outputs = torch.cat([self.attention(x) for _ in range(self.num_heads)], dim=2)
-        return self.WO(outputs)
+        outputs = torch.cat([attention(x) for attention in self.attentions], dim=2)
+        return self.dropout(self.WO(outputs))
--- modules/transformer.py
+++ modules/transformer.py
@@ -1,6 +1,7 @@
-from torch import nn
 import torch
+from torch import nn
 from .self_attention import MultiHeadAttention
+import math
 
 class TransformerEncoderBlock(nn.Module):
     def __init__(self, embed_dim, num_heads) -> None:
@@ -9,28 +10,43 @@ class TransformerEncoderBlock(nn.Module):
         self.norm1 = nn.LayerNorm(embed_dim)
         self.norm2 = nn.LayerNorm(embed_dim)
         self.linear = nn.Linear(embed_dim, embed_dim)
-        self.bias = nn.Parameter(torch.zeros(embed_dim))
         
 
     def __call__(self, x):
         x = x + self.attention(self.norm1(x))
-        x = x + self.linear(self.norm2(x)) + self.bias
+        x = x + self.linear(self.norm2(x))
         return x
-    
+
+
+class NewGELU(nn.Module):
+    """
+    Implementation of the GELU activation function currently in Google BERT repo (identical to OpenAI GPT).
+    Reference: Gaussian Error Linear Units (GELU) paper: https://arxiv.org/abs/1606.08415
+    """
+    def forward(self, x):
+        return 0.5 * x * (1.0 + torch.tanh(math.sqrt(2.0 / math.pi) * (x + 0.044715 * torch.pow(x, 3.0))))
+
+
 
 class DecoderBlock(nn.Module):
-    def __init__(self, embed_dim, num_heads) -> None:
+    def __init__(self, embed_dim, num_heads, proba_dropout=0.1) -> None:
         super().__init__()
         self.attention = MultiHeadAttention(embed_dim, embed_dim, num_heads, masked=True)
         self.norm1 = nn.LayerNorm(embed_dim)
         self.norm2 = nn.LayerNorm(embed_dim)
         self.linear = nn.Linear(embed_dim, embed_dim)
-        self.bias = nn.Parameter(torch.zeros(embed_dim))
-        
+
+        # MLP
+        self.fc = nn.Linear(embed_dim, 4 * embed_dim)
+        # self.activation  = nn.ReLU()
+        self.activation  = NewGELU()
+        self.proj = nn.Linear(4 * embed_dim, embed_dim)
+        self.dropout = nn.Dropout(proba_dropout)
+        self.mlpf = lambda x: self.dropout(self.proj(self.activation(self.fc(x))))
 
     def __call__(self, x):
         x = x + self.attention(self.norm1(x))
-        x = x + self.linear(self.norm2(x)) + self.bias
+        x = x + self.mlpf(self.norm2(x))
         return x
 
     # def generate(self, x, max_length=50):   
